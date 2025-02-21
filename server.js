2b29d1a9f4a352d1ded2586ccf2dd9f054c5a1f7
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2025-02-21 14:08:05 +0800

    updated some formatting

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import WebSocket, {WebSocketServer} from 'ws';
 import {networkInterfaces} from 'os';
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
 const __filename=fileURLToPath(import.meta.url);
 const __dirname=path.dirname(__filename);
 const getLocalIP=()=>{
