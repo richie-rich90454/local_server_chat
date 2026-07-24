089104c10c86c4d218965a5f3512222606cf6905
richie-rich90454 <rjiang880@gmail.com>
2026-07-24 18:19:21 +0800

    Extract server HTTP into separate module
    Move Express app, static file serving, REST endpoints, and
    profanity filter into src/server/http.js. Export createHttpServer.

---
+++ src/server/http.js
@@ -0,0 +1,55 @@
+import express from"express";
+import path from"path";
+import{fileURLToPath}from"url";
+import{Filter}from"bad-words";
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+export function createHttpServer(localIP,portUI,portWS,serverName,joinCode,joinCodeMap){
+	const app=express();
+	app.use(express.json());
+	app.use(express.static(path.join(__dirname,"../../../dist"),{
+		maxAge:"1h",
+		etag:true,
+		lastModified:true,
+		setHeaders(res,filePath){
+			if(filePath.endsWith("sw.js")||filePath.endsWith("index.html")){
+				res.setHeader("Cache-Control","no-cache");
+			}
+		}
+	}));
+	app.get("/get-client-ip",(req,res)=>{
+		let ip=req.headers["x-forwarded-for"]||req.socket.remoteAddress;
+		if(ip&&ip.includes("::ffff:")){
+			ip=ip.split("::ffff:")[1];
+		}
+		res.json({ip:ip});
+	});
+	app.get("/server-info",(req,res)=>{
+		res.json({
+			name:serverName,
+			port:portWS,
+			ip:localIP,
+			joinCode:joinCode,
+			uiPort:portUI
+		});
+	});
+	app.get("/join-code/:code",(req,res)=>{
+		const info=joinCodeMap.get(req.params.code.toUpperCase());
+		if(info){
+			res.json({found:true,...info});
+		}
+		else{
+			res.json({found:false});
+		}
+	});
+	const filter=new Filter();
+	app.post("/check-name",(req,res)=>{
+		const {name}=req.body;
+		const isClean=!filter.isProfane(name);
+		res.json({clean:isClean});
+	});
+	const server=app.listen(portUI,()=>{
+		console.log(`UI on http://${localIP}:${portUI}`);
+	});
+	return{app,server};
+}
